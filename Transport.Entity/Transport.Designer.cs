2a492877f509dcb467c088e14b8f3292f90d4e1e
Santhana-Nivas <santhananivas.dci@gmail.com>
2026-04-14 21:39:14 +0530

    complete the data

--- Transport.Entity/Transport.Designer.cs
+++ Transport.Entity/Transport.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\HP\Documents\repos\Transport\Transport.Entity\Transport.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\DELL\Desktop\Project-Folder\JJK_Info\Nessi\Nessi_Travels_Razor\Transport.Entity\Transport.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
